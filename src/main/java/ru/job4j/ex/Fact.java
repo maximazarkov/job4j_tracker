3ab83b1968afdf46b4c87eb5cf58a2ba46be931b
Maxim Azarkov <aza-maxim@yandex.ru>
2021-08-23 00:12:51 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+
+    }
+
+    public int calc(int n) {
+
+        if (n < 0) {
+            throw new IllegalArgumentException("Error: n < 0");
+        }
+
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
