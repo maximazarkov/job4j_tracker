0e9fe3499dbfd78e2a556f55d4d3c059633b3e8a
Maxim Azarkov <aza-maxim@yandex.ru>
2021-08-27 02:16:57 +0700

    4. Статистика по тестам Codecove [#504789] - настройка Travis-ci

--- src/main/java/ru/job4j/tracker/tracker/Store.java
+++ src/main/java/ru/job4j/tracker/tracker/Store.java
@@ -5,9 +5,35 @@ import ru.job4j.tracker.Item;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Интерфейс для описания функциональности системы работы со статьями замечаний.
+ * Предназначена для возможно реализовать объекты для подключения к разным системам
+ * ввода/вывода и хранения статей без изменения основного кода программы.
+ */
 public interface Store extends AutoCloseable {
-    void init();
+    /**
+     * Метод необходим для настройки модулей, реализующих интерфейс Store,
+     * при необходимости. Например, предварительная настройка при подключении
+     * базы данных.
+     */
+    default void init() {
+
+    }
+
+    /**
+     * Метод добавляет объект Item.
+     * @param item объект статьи для добавления.
+     * @return возвращает объект статьи для дальнейшего использования.
+     */
     Item add(Item item);
+
+    /**
+     * Метод для замены (обновления) статьи.
+     * @param id id статьи
+     * @param item статья
+     * @return возвращает true, если данные удалось заменить; false - в случае неудачи
+     * @throws SQLException возможны исключения при работе с внешними системами.
+     */
     boolean replace(String id, Item item) throws SQLException;
     boolean delete(String id);
     List<Item> findAll() throws SQLException;
