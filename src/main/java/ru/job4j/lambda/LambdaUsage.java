06b69f438b924a11b044cba655c8a888b1b814e6
Maxim Azarkov <aza-maxim@yandex.ru>
2021-09-17 21:31:05 +0700

    2.2. Лямбда блок [#249208]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,2 +1,48 @@
-package ru.job4j.lambda;public class LambdaUsage {
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LambdaUsage {
+
+    public static void main(String[] args) {
+        Comparator<String> cmpText1 = (left, right) -> left.compareTo(right) ;
+        //или
+        Comparator<String> cmpText2 = String::compareTo;
+
+        Comparator<String> cmpDescSize = (left, right) -> (left.length() - right.length());
+        cmpDescSize = Comparator.reverseOrder();
+
+        String[] numbers = {
+                "1. Task",
+                "11. Task",
+                "2. Task"
+        };
+
+        Comparator<String> numStr = (left, right) -> {
+            int first = Integer.valueOf(left.substring(0, left.indexOf(".")));
+            int second = Integer.valueOf(right.substring(0, right.indexOf(".")));
+            return first - second;
+        };
+
+        //было
+        Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
+        // стало
+        Comparator<Attachment> comp = (left, right) -> {
+            System.out.println("compare - " + left.getSize() + " : " + right.getSize());
+            return left.getSize() - right.getSize();
+        };
+
+        Comparator<String> cmpDescSizeLog = (left, right) -> {
+            System.out.println((right.length() > left.length()) ? "right > left" : "right < left");
+            return right.length() - left.length();
+        };
+
+        Arrays.sort(numbers, cmpDescSizeLog);
+        for (String s : numbers) {
+            System.out.println(s);
+        }
+
+    }
 }
+
