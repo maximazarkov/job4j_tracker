fd4a6c7d6c93120f569072d0e75318e030d1a78a
Maxim Azarkov <aza-maxim@yandex.ru>
2023-03-18 22:56:21 +0700

    6.3. Статические вложенные классы [#504951]

---
+++ src/main/java/ru/job4j/oop/Calc.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Calc {
+    public static class Multiple {
+        private final int rsl;
+
+        public Multiple(int rsl) {
+            this.rsl = rsl;
+        }
+
+        public int getResult() {
+            return rsl;
+        }
+    }
+
+    public static Multiple getMult(int value) {
+        int result = value * value;
+        return new Multiple(result);
+    }
+
+    public static void main(String[] args) {
+        Calc.Multiple mult = Calc.getMult(3);
+        System.out.println("Квадрат числа равен: " + mult.getResult());
+    }
+}
