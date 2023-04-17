7634d1a1d9d6f8805704b46b59875d072e55bb6c
Maxim Azarkov <aza-maxim@yandex.ru>
2023-04-17 21:47:59 +0700

    14. Агрегатные методы min(), max(), count(), average(), sum() [#504928]

---
+++ src/main/java/ru/job4j/stream/MinExample.java
@@ -0,0 +1,52 @@
+package ru.job4j.stream;
+
+import ru.job4j.stream.mapto.Person;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class MinExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(4, 5, 1, 3, 2);
+        Optional<Integer> minEl = list.stream()
+                .min(Comparator.naturalOrder());
+        System.out.println(minEl.get());
+
+        long count = list.stream()
+                .filter(el -> el % 2 == 0)
+                .count();
+        System.out.println(count);
+
+        OptionalInt min = IntStream
+                .rangeClosed(1, 5)
+                .min();
+        System.out.println(min.getAsInt());
+
+        List<Person> people =   Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        Optional<Person> youngestPerson = people.stream()
+                .min(Comparator.comparing(Person::getAge));
+        int age = youngestPerson.get().getAge();
+        System.out.println(age);
+        Optional<Person> oldestPerson = people.stream()
+                .max(Comparator.comparing(Person::getAge));
+        int ageMax = oldestPerson.get().getAge();
+        System.out.println(ageMax);
+
+        OptionalDouble average = people.stream()
+                .mapToInt(Person::getAge)
+                .average();
+        double avg = average.getAsDouble();
+        System.out.println(avg);
+
+        int sum = people.stream()
+                .mapToInt(Person::getAge)
+                .sum();
+        System.out.println(sum);
+    }
+}
